c939242089ac1ea0bd96afdebdf05d2814e661ff
casonbobo <casonsbobo@gmail.com>
2024-01-03 11:15:31 -0600

    Added the next 2. Trying to figure out annotated returns

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+ #!/usr/bin/env python3
+"""This is a funny little comment for the sentence thing"""
+
+
+def concat(a: str, b: str) -> str:
+    """Little annotation"""
+    return a + b
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a comment for a function that returns the floor"""
+
+
+def floor(a: float) -> int:
+    """its right below you"""
+    return a
