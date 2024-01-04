5cd1e575838db68c818ee7b6db818d86cf5862ec
casonbobo <casonsbobo@gmail.com>
2024-01-04 12:43:08 -0600

    Typing issue

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This takes a str and a num to make a tup"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """This is a toople"""
+    kv_tuple: Tuple = (k, float(v) ** 2)
+    return kv_tuple
