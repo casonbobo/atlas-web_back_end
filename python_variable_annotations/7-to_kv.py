ae943296e60296337fe14af9e7c8d223e87f3eb3
casonbobo <casonsbobo@gmail.com>
2024-01-04 12:48:02 -0600

    Ugh

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """This takes a str and a num to make a tup"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """This is a toople"""
     kv_tuple: Tuple = (k, float(v) ** 2)
