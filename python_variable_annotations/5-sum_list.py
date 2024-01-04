bf0eaa73ff298820b3a75cfed8c494260482f9af
casonbobo <casonsbobo@gmail.com>
2024-01-04 11:10:55 -0600

    Oop. I was typing for v3.9 not older

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """A list of floats returns the sum of them all"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """This is a function that adds a list together"""
     total: float = 0
     for number in input_list:
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This is a comment for learning mixed types"""
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: int | float) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """add a mixed list together"""
     total: float = 0
     for number in mxd_list:
