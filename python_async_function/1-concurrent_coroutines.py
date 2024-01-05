245aac43a370cd11b779c6dbd7d6ca9007482467
casonbobo <casonsbobo@gmail.com>
2024-01-05 12:50:53 -0600

    This is so dumb

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,8 +4,8 @@ import asyncio
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """Will this work MrAtlas"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     output = await asyncio.gather(*tasks)
-    return output
+    return sorted(output)
