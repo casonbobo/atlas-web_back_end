4c4e36a67caddab142b2b619ec8790875554b942
casonbobo <casonsbobo@gmail.com>
2024-01-05 15:59:58 -0600

    whoops

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
+    """I forgot this comment"""
     start_time = time.time()
     runs = [asyncio.create_task(async_comprehension()) for _ in range(4)]
     await asyncio.gather(*runs)
