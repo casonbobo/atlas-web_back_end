a709bacd9c98a83123310057eeb4d7adefc87a87
casonbobo <casonsbobo@gmail.com>
2024-01-05 12:20:27 -0600

    Dumb changes for the checker

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import random
 
 
 async def wait_random(max_delay=10):
+    """This is a comment"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """learning concurrent"""
-wait_random = __import__("0-basic_async_syntax").wait_random
 import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int):
