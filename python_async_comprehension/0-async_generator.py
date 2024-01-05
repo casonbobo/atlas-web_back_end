d0b700f473d30b7929c9807b55b2e9c164d561b1
casonbobo <casonsbobo@gmail.com>
2024-01-05 15:57:50 -0600

    I hope this is good. Please God

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """This is supposed to make coroutine loops"""
 import random
 import asyncio
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """This is for making async numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This set of tasks is pissing me off"""
+import asyncio
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    start_time = time.time()
+    runs = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*runs)
+    end_time = time.time()
+    return end_time - start_time
