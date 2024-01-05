c35ccd43ddf39870c32bf12a580546454522d2ba
casonbobo <casonsbobo@gmail.com>
2024-01-05 13:01:03 -0600

    maybe this will work

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """learning concurrent"""
 import asyncio
+from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Will this work MrAtlas"""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    output = await asyncio.gather(*tasks)
+    tasks = [await wait_random(max_delay) for _ in range(n)]
     return sorted(output)
