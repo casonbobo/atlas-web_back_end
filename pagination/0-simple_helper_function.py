91061ebd086b9b43e852a3a5d7109b6b8137159b
casonbobo <casonsbobo@gmail.com>
2024-01-10 11:06:02 -0600

    Ok I fixed my silly mistakes

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,13 +2,10 @@
 """This is supposed to be a simple helper function"""
 
 
-
-def index_range(page, page_size) {
+def index_range(page, page_size):
     """Honestly the instructions on this task made no sense"""
     start_index = (page - 1) * page_size
 
     end_index = start_index + page_size
 
     return (start_index, end_index)
-}
-
