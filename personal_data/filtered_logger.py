1b9f7b786ffed33d622a88e13d0014f51b8f91d5
casonbobo <casonsbobo@gmail.com>
2024-01-15 12:45:49 -0600

    fixed my errors

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """
     This is a file that will be added to over the course of the project.
-    This project is based around learning data obfuscation and encryption through Atlas
+    This project is based around learning data obfuscation
+    and encryption through Atlas
     I do not know what I am doing
 """
 import re
-import list
+import typing
 
 
-def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: list[str], redaction: str,
+                 message: str, separator: str) -> str:
     pattern = '|'.join(fields)
     obfuscated_message = re.sub(pattern, redaction, message)
     return obfuscated_message
