3d38ec1cf1c391d6a3481759a5ba6871540659c8
casonbobo <casonsbobo@gmail.com>
2024-01-17 12:17:49 -0600

    Had to add one extra line

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,6 +7,7 @@ def hash_password(password: str) -> bytes:
     """Encrypting passwords"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """This fuction validates the password and decodes it"""
     password = password.encode('utf-8')
