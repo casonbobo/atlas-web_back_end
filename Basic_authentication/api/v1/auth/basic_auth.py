30bf864e7e5ee749a4fff4a66c6d7b1db489f750
casonbobo <casonsbobo@gmail.com>
2024-01-17 15:47:02 -0600

    THis should be 7. Lets not break 5 and 6 again

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class, inherits from Auth"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> Optional[str]:
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ', 1)[1]
