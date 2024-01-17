e7959f01258ecdf5cb3a9e38dfc7eb40c640bb1f
casonbobo <casonsbobo@gmail.com>
2024-01-17 13:54:45 -0600

    Lovin sand

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,11 +7,11 @@ User = TypeVar('User')
 
 class Auth:
     """This is the template for the auth struct"""
-   def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-       return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
 
-   def authorization_header(self, request=None) -> Optional[str]:
-       return None
+    def authorization_header(self, request=None) -> Optional[str]:
+        return None
 
-   def current_user(self, request=None) -> Optional[User]:
-       return None
+    def current_user(self, request=None) -> Optional[User]:
+        return None
