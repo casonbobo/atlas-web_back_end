2b29c9849dc05dd2ab0544dd34e70e1253c458c1
casonbobo <casonsbobo@gmail.com>
2024-01-17 15:05:36 -0600

    Added to 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -42,6 +42,21 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.before_request
+def before_request():
+   if auth is None:
+       return
+
+   excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+   if request.path not in excluded_paths:
+       if not auth.require_auth(request.path, excluded_paths):
+           return
+   if auth.authorization_header(request) is None:
+       abort(401)
+   if auth.current_user(request) is None:
+       abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
