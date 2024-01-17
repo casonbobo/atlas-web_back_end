759a5d3a88d7abe4d4d2460386c4ae5e40593002
casonbobo <casonsbobo@gmail.com>
2024-01-17 15:10:48 -0600

    fixed indentation stuff

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -45,18 +45,18 @@ def not_found(error) -> str:
 @app.before_request
 def before_request():
     """This should find unauthorized handles"""
-   if auth is None:
-       return
+    if auth is None:
+        return
 
-   excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                      '/api/v1/forbidden/']
-   if request.path not in excluded_paths:
-       if not auth.require_auth(request.path, excluded_paths):
-           return
-   if auth.authorization_header(request) is None:
-       abort(401)
-   if auth.current_user(request) is None:
-       abort(403)
+    if request.path not in excluded_paths:
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
