7caf2c676c6503ba72218002164e784fcbbba263
casonbobo <casonsbobo@gmail.com>
2024-01-17 12:16:29 -0600

    3 didn't work but thats ok

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Encrypting passwords"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This fuction validates the password and decodes it"""
+    password = password.encode('utf-8')
+    return bcrypt.checkpw(password, hashed_password)
