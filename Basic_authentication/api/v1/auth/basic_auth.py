523da49135708656c198fd5ca8a294ebbdc9a428
casonbobo <casonsbobo@gmail.com>
2024-01-17 16:55:00 -0600

    task 8 v1

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Redux Auth class but now its basic"""
 
 from api.v1.auth.auth import Auth
+import Base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        if base64_authorization_header is None
+        or not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+
+        return decoded_bytes.decode('utf-8')
