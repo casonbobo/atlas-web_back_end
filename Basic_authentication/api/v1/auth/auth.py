05ce5ea2afd0c3a7d8b66fe61a53efe2d0731613
casonbobo <casonsbobo@gmail.com>
2024-01-17 14:26:10 -0600

    Maybe this will be ok

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -5,11 +5,22 @@ from flask import request
 
 User = TypeVar('User')
 
+
 class Auth:
     """This is the template for the auth struct"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """This is check the auth for the path"""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        normal_path = path if path.endswith("/") else path + "/"
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/"):
+                if normal_path == excluded_path:
+                    return False
+            else:
+                if path.startwith(excluded_path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> Optional[str]:
         """This is to get the auth header"""
