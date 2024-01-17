86627f8303fab9ca367f753812e6a9e4e08ea5a6
casonbobo <casonsbobo@gmail.com>
2024-01-17 14:46:49 -0600

    mayhaps mayhaops

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,7 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = getenv("AUTH_TYPE")
 
+if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,1 +1,8 @@
-!#/usr/bin/env python3
+#!/usr/bin/env python3
+"""Redux Auth class but now its basic"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class, inherits from Auth"""
