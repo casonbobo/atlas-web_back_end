c178c392abc63251b53e25e9dbb9218e15e57580
casonbobo <casonsbobo@gmail.com>
2024-01-22 13:13:27 -0600

    commit

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -51,13 +51,12 @@ def before_request():
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
-        if not auth.require_auth(request.path, excluded_paths):
-            return
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
