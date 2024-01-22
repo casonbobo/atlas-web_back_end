a54b7ae8a6c904bb11a2588bd37455fa4e101452
casonbobo <casonsbobo@gmail.com>
2024-01-22 11:53:23 -0600

    Added 10 and 11

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,8 +10,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self, authorization_header:
                                             str) -> str:
         """This returns the Base64 decoded"""
-        if authorization_header is None or not isinstance(authorization_header,
-                                                          str):
+        if authorization_header is None or \
+                not isinstance(authorization_header, str):
             return None
 
         if not authorization_header.startswith('Basic '):
@@ -45,3 +45,41 @@ class BasicAuth(Auth):
             user_email, user_password = \
                 decoded_base64_authorization_header.split(':')
             return user_email, user_password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) \
+            -> TypeVar('User'):
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user_list = User.search(user_email)
+        if len(user_list) == 0:
+            return None
+
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        if request is None:
+            return None
+
+        authorization_header = self.authorization_header(request)
+        if authorization_header is None:
+            return None
+
+        decoded_base64_authorization_header = \
+            self.decode_base64_authorization_header(authorization_header)
+        if decoded_base64_authorization_header is None:
+            return None
+
+        user_email, user_pwd = \
+            self.extract_user_credentials(decoded_base64_authorization_header)
+        if user_email is None or user_pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        return user
