ddfcad26d0c6b9fa3a6224181bfe84741faf456d
casonbobo <casonsbobo@gmail.com>
2024-01-22 19:42:37 -0600

    added an import. ugh

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """validate if everything inherits correctly without any overloading
     validate the “switch” by using environment variables"""
+from api.v1.auth.auth import Auth
+
 
 class SessionAuth(Auth):
     """This is a new structure for Auth"""
