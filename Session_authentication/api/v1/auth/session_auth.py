4a75675d60da914cc5b6a2cc636aad7ada6d90fd
casonbobo <casonsbobo@gmail.com>
2024-01-23 10:53:42 -0600

    added to session auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,20 @@
 """validate if everything inherits correctly without any overloading
     validate the “switch” by using environment variables"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """This is a new structure for Auth"""
-    pass
+    def __init__(self):
+        """attributes"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_idass
