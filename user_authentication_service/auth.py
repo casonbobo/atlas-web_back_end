9ce33f2603d7031d0c605f4d6e389e4d7513e9b9
casonbobo <casonsbobo@gmail.com>
2024-01-24 20:18:24 -0600

    fixed an indent

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -35,7 +35,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user and bcrypt.checkpw(password.encode('utf-8'),
-                                        user.hashed_password):
+                                       user.hashed_password):
                 return True
         except NoResultFound:
             pass
