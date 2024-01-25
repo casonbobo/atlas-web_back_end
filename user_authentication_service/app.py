8d83997fcf825738b72e5d3c00e19645e0fc9b2b
casonbobo <casonsbobo@gmail.com>
2024-01-24 21:40:02 -0600

    More fixes

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -47,17 +47,18 @@ def login():
         abort(401)
 
 
-@sessions_bp.route("/", methods=["DELETE"])
+@app.route("/", methods=["DELETE"])
 def logout():
     """logout method"""
     session_id = request.cookies.get('session_id', None)
     user = AUTH.get_user_from_session_id(session_id)
-    
+
     if session_id is None or user is None:
         abort(403)
 
     AUTH.destroy_session(user.id)
     return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
