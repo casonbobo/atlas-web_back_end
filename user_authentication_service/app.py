3292c821f67b73fefaad8dd2984667a7937ef08e
casonbobo <casonsbobo@gmail.com>
2024-01-24 22:00:57 -0600

    should be profile

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -60,5 +60,17 @@ def logout():
     return redirect('/')
 
 
+@profile_bp.route("/", methods=["GET"])
+def profile():
+    """profile structure"""
+    session_id = request.cookies.get('session_id', None)
+    user = AUTH.get_user_from_session_id(session_id)
+    
+    if session_id is None or user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
