cd92f82c441a42d235ed266ee0eae84814625503
casonbobo <casonsbobo@gmail.com>
2024-01-24 19:46:04 -0600

    Should be a functioning 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -13,5 +13,18 @@ def welcome():
     """basic Flask app"""
     return jsonify({"message": "Bienvenue"})
 
+@users_bp.route("/", methods=["POST"])
+def users():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    if not email or not password:
+        raise BadRequest("Missing email or password")
+    try:
+        user = auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
