ec60d87fa2d0099820db9a645d44ae25954ea497
casonbobo <casonsbobo@gmail.com>
2024-01-24 21:16:40 -0600

    changed to a try

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -52,11 +52,11 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """Get a user from session"""
-        if session_id is None:
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
             return None
-
-        user = self._db.find_user_by(session_id=session_id)
-        return user
 
 
 def _hash_password(password: str) -> bytes:
