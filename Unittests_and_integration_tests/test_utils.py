8fd6db1f0c36fe88066f90810aca1a0356d3e270
casonbobo <casonsbobo@gmail.com>
2024-01-29 17:25:55 -0600

    maybe 1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,5 +16,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """test access nested map for utils"""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
+    @parameterized.expand([
+        ({}, ("a",), "'a' is not found"),
+        ({"a": 1}, ("a", "b"), "'b' is not found")
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         exception_message):
+        """test meant to throw exception"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), exception_message)
+
 if __name__ == '__main__':
     unittest.main()
