e8312a075d305af451a2f4069e30f912e9d62ee7
casonbobo <casonsbobo@gmail.com>
2024-01-30 15:16:58 -0600

    Added classes methods

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,9 +2,10 @@
 """tests for the GithubOrg client class"""
 from client import GithubOrgClient
 import unittest
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from unittest.mock import patch, PropertyMock
 from requests.exceptions import HTTPError
+import fixtures
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -14,21 +15,25 @@ class TestGithubOrgClient(unittest.TestCase):
         ('abc')
     ])
     def test_org(self, org):
+        """test the org"""
         pass
 
     def test_public_repos_url(self):
+        """test public repos"""
         pass
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
+        """test public repos"""
         pass
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
-    def test_has_license(self, repo, key, expected_output):
-        pass
+    def test_has_license(self, repo, key, expected):
+        """test has license"""
+        self.assertEqual(GithubOrgClient.has_license(repo, key), expected)
 
 
 class TestIntegrationGithubOrgClient(unittest.TestCase):
@@ -36,11 +41,17 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        """Set Up class"""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
+        cls.mock_get.return_value.json.side_effect \
+            = [cls.org_payload, cls.repos_payload]
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        """Tear Down class"""
+        cls.get_patcher.stop()
 
     def test_public_repos(self):
+        """test public repos"""
         pass
