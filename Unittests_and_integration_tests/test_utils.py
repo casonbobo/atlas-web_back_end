19051ae20dfc67ea01d69afbe6f63268c94b8b47
casonbobo <casonsbobo@gmail.com>
2024-01-29 18:25:35 -0600

    This is 2. Thank goodness for local testing

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """TestAccessNestedMap class"""
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,15 +17,27 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
     @parameterized.expand([
-        ({}, ("a",), "'a' is not found"),
-        ({"a": 1}, ("a", "b"), "'b' is not found")
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(self, nested_map, path,
-                                         exception_message):
+    def test_access_nested_map_exception(self, nested_map, path):
         """test meant to throw exception"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-            self.assertEqual(str(context.exception), exception_message)
+
+class TestGetJSON(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('requests.get', return_value=mock_response):
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_response.json.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
