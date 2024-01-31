d7414636c6ac43f7398e5e66825a2c5feecad727
casonbobo <casonsbobo@gmail.com>
2024-01-31 13:03:07 -0600

    had to make some changes for the checks

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
-	return 'Hello World'
+    """the function that is run with a flask app"""
+    return 'Hello world'
+
 
 # main driver function
 if __name__ == '__main__':
-	app.run()
+    app.run()
