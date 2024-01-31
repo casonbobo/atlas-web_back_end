e80c659e0aec601b3989e095b00c3e1abcf63be4
casonbobo <casonsbobo@gmail.com>
2024-01-31 15:41:35 -0600

    structure for 5 6 7

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -3,6 +3,14 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 app = Flask(__name__)
 
 
