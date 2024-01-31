fd3f6f2ba50396d58690f178b59558ee6346d874
casonbobo <casonsbobo@gmail.com>
2024-01-31 13:10:16 -0600

    ok this should work now

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """A basic flask app"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
     """the function that is run with a flask app"""
-    return 'Hello world'
+    return render_template('0-index.html')
 
 
 # main driver function
