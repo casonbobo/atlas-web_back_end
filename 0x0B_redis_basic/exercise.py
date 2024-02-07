4bd9016176eed9c66a78ee11e6d3975f765ea731
casonbobo <casonsbobo@gmail.com>
2024-02-07 14:56:43 -0600

    added three methods

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -16,3 +16,22 @@ class Cache:
         key = str(uuid4)
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, float]:
+        """take the str arg and callable"""
+        data = self._regis.get(key)
+        if fn:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str) -> str:
+        """turn into string"""
+        value = self.get(key, str)
+        return value
+
+    def get_str(self, key: str) -> int:
+        """turn into int"""
+        value = self.get(key, int)
+        return value
