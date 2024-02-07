fc40e59ecbeb4823bee5d7651b2c3431026b0d2d
casonbobo <casonsbobo@gmail.com>
2024-02-07 14:17:14 -0600

    added comments and fixed my shebang

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,15 +1,18 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """Cache class stores an instance of the Redis client"""
 import redis
 from uuid import uuid4
 from typing import Union
 
 class Cache:
+    """Cache class for Regis"""
     def __init__(self):
+        """Init"""
         self.__redis = redis.Redis()
         self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Key method"""
         key = str(uuid4)
         self.__redis.set(key, data)
         return key
