3f5026539b95e616d402106e71d887f12cc05ee2
casonbobo <casonsbobo@gmail.com>
2024-02-09 12:02:26 -0600

    I believe this should be ok

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """Cache class stores an instance of the Redis client"""
 import redis
+import functools
 from uuid import uuid4
 from typing import Union, Callable
+
+
+def count_call(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(*args, **kwargs):
+        instance = args[0]
+        method_qualname = method.__qualname__
+        if method_qualname not in instance.call_counts:
+            instance.call_counts[method_qualname] = 0
+        instance.call_counts[method_qualname] += 1
+        return method(*args, **kwargs)
+    return wrapper
+
 
 class Cache:
     """Cache class for Regis"""
@@ -11,13 +25,14 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_call
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Key method"""
         key = str(uuid4)
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = 
+    def get(self, key: str, fn: Callable =
             None) -> Union[str, bytes, int, float]:
         """take the str arg and callable"""
         data = self._redis.get(key)
