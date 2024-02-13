0aec0f208c53aa339e0170bb6019e92ac610bac4
casonbobo <casonsbobo@gmail.com>
2024-02-13 14:43:00 -0600

    Added the files for 4

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+CREATE TRIGGER decrease_item_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE item_id = NEW.item_id;
+END;
+//
+DELIMITER ;
