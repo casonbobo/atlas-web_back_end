20c2442b5e544684cdefbdb6e701909b197ad996
casonbobo <casonsbobo@gmail.com>
2024-02-13 12:35:04 -0600

    made table and database

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---create table of unique users
----with id, email, and name
+-- create table of unique users
+-- with id, email, and name
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
