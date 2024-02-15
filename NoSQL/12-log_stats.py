b09bc28a0134b135e8a811ebe96aeeee2798cdfe
casonbobo <casonsbobo@gmail.com>
2024-02-14 23:23:03 -0600

    maybe 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a Python script that provides some 
+stats about Nginx logs stored in MongoDB
+"""
+
+def log():
+    """provides some stats"""
+    pass
+
+if __name__ == "__main__":
+    log()
