a70af4b3152cafbf26e9b8ea59c586df95c9ad09
casonbobo <casonsbobo@gmail.com>
2024-02-14 22:45:09 -0600

    7 8 and 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+"""
+Write a function that inserts a new document 
+in a collection based on kwargs
+"""
+def insert_school(mongo_collection, **kwargs):
+    new_id = mongo_collection.insert_one(kwargs).inserted_id
+    return new_id
