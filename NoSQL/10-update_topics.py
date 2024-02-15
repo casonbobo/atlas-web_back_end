bab1e9b02f0c5e6b90f9065e8519b4de14d536d3
casonbobo <casonsbobo@gmail.com>
2024-02-14 23:24:19 -0600

    bin env swapped

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,4 +1,4 @@
-#!/usr/env/bin python3
+#!/usr/bin/env python3
 """Write a Python function that inserts a new document in a collection based"""
 
 def update_topics(mongo_collection, name, topics):
--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,4 +1,4 @@
-#!/usr/env/bin python3
+#!/usr/bin/env python3
 """Write a function that returns the list of school having a specific topic"""
 
 def schools_by_topic(mongo_collection, topic):
