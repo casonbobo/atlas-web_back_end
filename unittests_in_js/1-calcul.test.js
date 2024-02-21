6818463681f28e4fcf6fe60d2bbbd0519d116303
casonbobo <casonsbobo@gmail.com>
2024-02-21 16:39:45 -0600

    Added task 1 and make a new calc

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,19 @@
+// Task 0 test cases
+
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', () => {
+  it('adding 1 and 3, return 4', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+  });
+  it('adding 1 and 3.7, return 5', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 4.2, 3.7), 0);
+  });
+  it('adding 1.2 and 3.7, return 5', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 8, 1), 8);
+  });
+  it('adding 1.5 and 3.7, return 6', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
+  });
+})
