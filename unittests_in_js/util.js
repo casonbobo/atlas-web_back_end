3a8b48e7c14394e2dd3f3968820f7e2e43f79141
casonbobo <casonsbobo@gmail.com>
2024-02-23 14:55:08 -0600

    I am not even sure what this does totally

---
+++ unittests_in_js/util.js
@@ -0,0 +1,22 @@
+const Utils = {
+  calculateNumber: function(type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+  
+    if (type === 'SUM') {
+      return a + b;
+    } else if (type === 'SUBTRACT') {
+      return a - b;
+    } else if (type === 'DIVIDE') {
+      if (b ===  0) {
+        return 'Error';
+      } else {
+        return a / b;
+      }
+    } else {
+      return 'Invalid operation';
+    }
+  }
+};
+
+module.exports = Utils;
