48faf31918dee899edf845212c4b80cd2e9a60f0
casonbobo <casonsbobo@gmail.com>
2024-02-27 15:32:27 -0600

    fixed the lint issues

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
 // Create a small http sever using the http module
 
 const http = require('http');
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Hello Holberton School!");
+
+const app = http.createServer((res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
