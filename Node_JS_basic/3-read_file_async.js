10bf3c3a9b536313c238ff9a44fbb94f642bf2ec
casonbobo <casonsbobo@gmail.com>
2024-02-27 15:02:56 -0600

    I can't run eslint...

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,16 +3,16 @@ const fs = require('fs').promises;
 async function countStudents(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== ''); // Filter out empty lines
+    const lines = data.split('\n').filter(line => (line.trim() !== '')); // Filter out empty lines
     const fields = lines[0].split(','); // Assuming the first line contains field names
     const studentData = lines.slice(1); // Skip the header line
 
-    let totalStudents =  0;
-    console.log("Number of students: " + studentData.length);
+    let totalStudents = 0;
+    console.log(`Number of students: ${studentData.length}`);
 
     fields.forEach((field, index) => {
-      const studentsInField = studentData.filter(line => line.split(',')[index]).length;
-      const studentNames = studentData.filter(line => line.split(',')[index]).map(line => line.split(',')[0]);
+      const studentsInField = studentData.filter(line => (line.split(',')[index]).length);
+      const studentNames = studentData.filter(line => (line.split(',')[index])).map(line => (line.split(',')[0]));
       console.log(`Number of students in ${field}: ${studentsInField}. List: ${studentNames.join(', ')}`);
       totalStudents += studentsInField;
     });
