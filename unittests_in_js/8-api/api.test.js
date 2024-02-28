02c77c558e284ff0ea8a4ff12e43dbee8ebb6137
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-02-28 14:56:23 -0600

    None of this works and I am going crazy

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,16 +1,18 @@
-const { expect } = require('chai');
+//Basic integration tests
+
 const request = require('request');
+const expect = require('chai').expect;
 
 describe('Index page', function() {
-  it('returns status code 200', function(done){
-    request('http://localhost:7865', function(error, reposnse, body){
+  it('returns status code 200', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('returns correct result', function(done) {
-    request('http://localhost:7865', function(error, response, body){
+    request('http://localhost:7865', function(error, response, body) {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', function() {
+  it('returns status code 200', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('returns correct result', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+  
+describe('Cart page', function() {
+  it('returns status code 200 when :id is a number', function(done){
+    request('http://localhost:7865/cart/12', function(error, response, body){
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('returns status code 404 when :id is NOT a number', function(done) {
+    request('http://localhost:7865/cart/hello', function(error, response, body){
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
