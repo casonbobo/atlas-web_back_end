9197fc586298b3ad43457d6dcea5924d9c9d1c5e
casonbobo <casonsbobo@gmail.com>
2024-02-27 22:57:07 -0600

    not being able to run eslint is gonna make me crazy

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,10 +1,11 @@
 // learning spies sorta
+
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${sum}`);
-  return sum
+  return sum;
 }
 
 module.exports = sendPaymentRequestToApi;
