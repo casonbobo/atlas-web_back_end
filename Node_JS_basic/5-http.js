9228859fe3bba9bd63288bbde7a6a1134599bedc
casonbobo <casonsbobo@gmail.com>
2024-02-27 22:42:27 -0600

    maybe now

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const url = require('url');
 
 // Function to read the CSV file asynchronously
-const readFileAsync = ((filePath) => {
+const readFileAsync = filePath => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
@@ -15,7 +15,7 @@ const readFileAsync = ((filePath) => {
       }
     });
   });
-});
+};
 
 // Function to handle requests and send responses
 const handleRequest = async (req, res) => {
